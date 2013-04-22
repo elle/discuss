2329b58519e95df69e53911a927bd9ac63fe07a0
Elle Meredith <ellemeredith@gmail.com>
2013-04-22 11:37:25 +1000

    minitest reporters rspec style

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,16 @@
 # Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] = 'test'
 
 require 'minitest/autorun'
 require 'minitest/spec'
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-require "rails/test_help"
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require 'rails/test_help'
+
+require 'minitest/reporters'
+MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir['#{File.dirname(__FILE__)}/support/**/*.rb'].each { |f| require f }
