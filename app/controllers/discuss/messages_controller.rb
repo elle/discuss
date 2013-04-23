5e4483ec26351e9434e5ec210b1a4ef99a456d64
Elle Meredith <ellemeredith@gmail.com>
2013-04-23 14:59:57 +1000

    starting on views [wip]

--- app/controllers/discuss/messages_controller.rb
+++ app/controllers/discuss/messages_controller.rb
@@ -3,15 +3,13 @@ require_dependency 'discuss/application_controller'
 module Discuss
   class MessagesController < ApplicationController
     before_action :check_mailbox_params, only: :index
+    before_action :message, only: [:show, :update, :destroy]
 
     def index
       @messages = Message.send(params[:mailbox], user)
     end
 
     def show
-    end
-
-    def new
     end
 
     def create
@@ -29,6 +27,11 @@ module Discuss
       @user ||= DiscussUser.find_by(user_id: current_discuss_user.id, user_type: current_discuss_user.class.to_s)
     end
     helper_method :user
+
+    def message
+      @message ||= Message.find(params[:id])
+    end
+    helper_method :message
 
     def check_mailbox_params
       redirect_to 'messages/inbox' unless valid_mailbox?
