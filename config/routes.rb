bcd7c4ca59ea81265d08318e9f55f0e12396f483
Garrett Heinlen <heinleng@gmail.com>
2013-04-24 15:08:06 +1000

    Adjusting routes to ensure normal message resource does not interfere with the mailbox/inbox actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Discuss::Engine.routes.draw do
 
   get 'messages/compose', to: 'messages#new', as: :compose
-  get 'messages/:mailbox', to: 'messages#index', as: :mailbox
   resources :messages
+  get 'mailbox/:mailbox', to: 'messages#index', as: :mailbox
 end
