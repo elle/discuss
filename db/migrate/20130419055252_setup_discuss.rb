f826f55265f6dc48d734a282128758ee1823303d
Garrett Heinlen <heinleng@gmail.com>
2013-04-24 11:01:04 +1000

    Adding migrations into the gem itself so that rails will detect this as a true _engine_ and allow for the rake task to copy migrations into peoples projects

---
+++ db/migrate/20130419055252_setup_discuss.rb
@@ -0,0 +1,39 @@
+class SetupDiscuss < ActiveRecord::Migration
+
+  def change
+    create_table :messages do |t|
+      t.string      :subject
+      t.text        :body
+      t.integer     :sender_id
+      t.integer     :parent_id
+      t.boolean     :draft,      default: false
+      t.datetime    :trashed_at
+      t.datetime    :deleted_at
+
+      t.timestamps
+    end
+    add_index :messages, :sender_id
+
+    create_table :message_users do |t|
+      t.references :message
+      t.references :discuss_user
+      t.datetime   :trashed_at
+      t.datetime   :deleted_at
+      t.datetime   :read_at
+
+      t.timestamps
+    end
+    add_index :message_users, :message_id
+    add_index :message_users, :discuss_user_id
+
+    create_table :discuss_users do |t|
+      t.references :user, polymorphic: true, default: 'User'
+      t.string     :email
+      t.string     :name
+
+      t.timestamps
+    end
+    add_index :discuss_users, [:user_id, :user_type]
+
+  end
+end
