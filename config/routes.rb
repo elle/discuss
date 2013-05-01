64172e6ac450abe5082b10c647322ca508a0b50d
Elle Meredith <ellemeredith@gmail.com>
2013-05-01 13:39:16 +1000

    adjusting routes to have a singular path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Discuss::Engine.routes.draw do
 
-  get '/messages/compose', to: 'messages#new', as: :compose
 
-  resources :messages do
+  resources :messages, except: :index, path: 'message' do
+    get 'compose',     to: 'messages#new', as: :compose
     post 'save_draft', to: 'messages#save_draft', as: :save_draft
   end
 
