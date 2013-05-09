934d457e11e2f31b9a1179c5cedcd17c6d8acc97
Elle Meredith <ellemeredith@gmail.com>
2013-05-09 16:41:23 +1000

    this route is not used anymore

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Discuss::Engine.routes.draw do
 
   get 'message/compose', to: 'messages#new', as: :compose
 
-  resources :messages, except: :index, path: 'message' do
-    post 'save_draft', to: 'messages#save_draft', as: :save_draft
-  end
+  resources :messages, except: :index, path: 'message'
 
   get 'mailbox/:mailbox', to: 'mailboxes#show', as: :mailbox
 
