43514d316b396e7f8765e3d55a87d01549105185
Brenton Annan <brentonannan@brentonannan.com>
2014-07-31 10:35:44 +1000

    Map .all onto all discussable classes for recipients list

--- app/controllers/discuss/application_controller.rb
+++ app/controllers/discuss/application_controller.rb
@@ -3,15 +3,24 @@ module Discuss
     before_action :discuss_current_user
 
     private
+
     def discuss_current_user
       current_user
     end
     helper_method :discuss_current_user
 
     def recipients
-      discuss_recipients || User.all.reject { |u| u == discuss_current_user }
+      discuss_recipients || recipient_objects.reject { |u| u == discuss_current_user }
     end
     helper_method :recipients
+
+    def recipient_objects
+      recipient_classes.map { |c| c.all }.flatten
+    end
+
+    def recipient_classes
+      ActiveRecord::Base.descendants.select { |c| c.included_modules.include? Discuss::Models::Discussable }
+    end
 
     # For example `set_flash_message :notice, :trash_emptied`
     def set_flash_message(key, kind, options = {})
